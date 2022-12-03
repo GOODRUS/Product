6ca65c7291dad25ae054925ef0f4cfd3b1bf880d
Dmitriy Shilyaev <goodrus1993@yandex.ru>
2022-12-03 13:26:19 +0500

    More Test fix

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -79,10 +79,11 @@ public class ProductManagerTest {
         manager.add(book1);
         manager.add(book2);
         manager.add(smartphone1);
+        manager.add(smartphone2);
         manager.add(smartphone3);
 
-        Product[] expected = {book2};
-        Product[] actual = manager.searchBy("Война и мир");
+        Product[] expected = {smartphone2, smartphone3};
+        Product[] actual = manager.searchBy("Sony");
 
         assertArrayEquals(expected, actual);
     }
@@ -95,6 +96,9 @@ public class ProductManagerTest {
     public void shouldSearchOnlyOneProduct() {
 
         manager.add(book1);
+        manager.add(book2);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
 
         Product[] expected = {book1};
         Product[] actual = manager.searchBy("Тараканище");
@@ -103,14 +107,37 @@ public class ProductManagerTest {
     }
 
     /*
-    Тест на метод поиска "searchBy" при ситуации когда не добавлено ни одного товара
+    Тест на метод поиска "searchBy" при ситуации когда ни один товар не подходит
      */
 
     @Test
     public void shouldSearchNoProducts() {
 
+        manager.add(book2);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+        manager.add(smartphone2);
+
         Product[] expected = {};
         Product[] actual = manager.searchBy("Тараканище");
+
+        assertArrayEquals(expected, actual);
+    }
+
+     /*
+    Тест на метод поиска "searchBy" при ситуации когда находится несколько одинаковых товаров
+     */
+
+    @Test
+    public void shouldSearchSameProducts() {
+
+        manager.add(book2);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+        manager.add(smartphone2);
+
+        Product[] expected = {smartphone2, smartphone2};
+        Product[] actual = manager.searchBy("Sony Xperia Z3");
 
         assertArrayEquals(expected, actual);
     }
